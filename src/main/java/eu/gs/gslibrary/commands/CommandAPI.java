885cde07134cce7837be9c25600afa107f85cf71
Gennario <48251906+Gennario@users.noreply.github.com>
2022-08-08 21:28:58 +0200

    Some language and commands changes

--- src/main/java/eu/gs/gslibrary/commands/CommandAPI.java
+++ src/main/java/eu/gs/gslibrary/commands/CommandAPI.java
@@ -110,7 +110,7 @@ public class CommandAPI {
                 if (args.length == 0) {
                     if (help) {
                         sender.sendMessage(languageAPI.getMessage("commands.usage", null, new Replacement((player, string) -> {
-                            return string.replace("%label%", label).replace("%usage%", "help [page]");
+                            return string.replace("%label%", label).replace("%help%", "help [page]");
                         })).toArray(new String[0]));
                         return true;
                     } else if (emptyCommandResponse != null) {
@@ -182,7 +182,7 @@ public class CommandAPI {
 
                             if(required > commandArgs.size() || commandArgs.isEmpty() || commandArgs.size() > command.getSubCommandArgs().size()) {
                                 sender.sendMessage(languageAPI.getMessage("commands.usage", null, new Replacement((player, string) -> {
-                                    return string.replace("%label%", label).replace("%usage%", command.getUsage());
+                                    return string.replace("%label%", label).replace("%help%", command.getUsage());
                                 })).toArray(new String[0]));
                                 return true;
                             }
